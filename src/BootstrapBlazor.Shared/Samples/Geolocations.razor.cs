9994289d8ff03ebd2557421e5deb445f93f62b2b
Argo-Lenovo <argo@163.com>
2022-02-08 12:30:15 +0800

    refactor: 重命名 GeolocationItem

--- src/BootstrapBlazor.Shared/Samples/Geolocations.razor.cs
+++ src/BootstrapBlazor.Shared/Samples/Geolocations.razor.cs
@@ -37,8 +37,8 @@ public sealed partial class Geolocations
     private IStringLocalizer<Geolocations>? Localizer { get; set; }
 
     private string? status { get; set; }
-    private Geolocationitem? geolocations { get; set; }
-    private List<Geolocationitem> Items { get; set; } = new List<Geolocationitem>() { new Geolocationitem() };
+    private GeolocationItemT? geolocations { get; set; }
+    private List<GeolocationItemT> Items { get; set; } = new List<GeolocationItemT>() { new GeolocationItemT() };
 
     private string ImageUrl(string image) => $"_content/BootstrapBlazor.Shared/images/{image}";
 
@@ -57,7 +57,7 @@ public sealed partial class Geolocations
     }
 
 
-    private Task OnResult(Geolocationitem geolocations)
+    private Task OnResult(GeolocationItemT geolocations)
     {
         this.geolocations = geolocations;
         Items[0] = geolocations;
