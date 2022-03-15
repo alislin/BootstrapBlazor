6d9308b673f2860b58fa5747f1199dd2e2e18125
Argo-Lenovo <argo@163.com>
2022-03-15 17:00:53 +0800

    test: 增加表单内单元测试

--- test/UnitTest/Components/CascaderTest.cs
+++ test/UnitTest/Components/CascaderTest.cs
@@ -8,4 +8,19 @@ namespace UnitTest.Components;
 
 public class CascaderTest : BootstrapBlazorTestBase
 {
+    [Fact]
+    public void ValidateForm_OK()
+    {
+        var foo = new Foo();
+        var cut = Context.RenderComponent<ValidateForm>(pb =>
+        {
+            pb.Add(a => a.Model, foo);
+            pb.AddChildContent<Cascader<string>>(pb =>
+            {
+                pb.Add(a => a.DisplayText, "Test_DisplayText");
+                pb.Add(a => a.ShowLabel, true);
+            });
+        });
+        cut.Contains("Test_DisplayText");
+    }
 }
