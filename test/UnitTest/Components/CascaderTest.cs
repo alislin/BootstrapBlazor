0bd20a1a3664a77852de689762a241f402f57800
Argo-Lenovo <argo@163.com>
2022-03-15 17:01:17 +0800

    test: 增加 Items 单元测试

--- test/UnitTest/Components/CascaderTest.cs
+++ test/UnitTest/Components/CascaderTest.cs
@@ -33,4 +33,54 @@ public class CascaderTest : BootstrapBlazorTestBase
         });
         cut.Contains("border-success");
     }
+
+    [Fact]
+    public async Task Items_Ok()
+    {
+        var value = "";
+        var selectedItems = new List<CascaderItem>();
+        var items = new List<CascaderItem>()
+        {
+            new() { Text = "Test1", Value = "1" },
+            new() { Text = "Test2", Value = "2" }
+        };
+        items[1].AddItem(new() { Text = "Test11", Value = "11" });
+        items[1].AddItem(new() { Text = "Test12", Value = "12" });
+
+        items[1].Items.ElementAt(1).AddItem(new() { Text = "Test111", Value = "111" });
+
+        var cut = Context.RenderComponent<Cascader<string>>(pb =>
+        {
+            pb.Add(a => a.Items, items);
+            pb.Add(a => a.Value, value);
+            pb.Add(a => a.OnValueChanged, v =>
+            {
+                value = v;
+                return Task.CompletedTask;
+            });
+            pb.Add(a => a.OnSelectedItemChanged, items =>
+            {
+                selectedItems.AddRange(items);
+                return Task.CompletedTask;
+            });
+        });
+
+        var dropdownItems = cut.FindAll(".dropdown-item");
+        await cut.InvokeAsync(() => dropdownItems[1].Click());
+        Assert.Equal("2", value);
+        Assert.Single(selectedItems);
+
+        dropdownItems = cut.FindAll(".dropdown-item");
+        await cut.InvokeAsync(() => dropdownItems[0].Click());
+        Assert.Equal("1", value);
+
+        // nav-link
+        var linkItems = cut.FindAll(".nav-link");
+        await cut.InvokeAsync(() => linkItems[0].Click());
+        Assert.Equal("11", value);
+
+        linkItems = cut.FindAll(".nav-link");
+        await cut.InvokeAsync(() => linkItems[1].Click());
+        Assert.Equal("12", value);
+    }
 }
