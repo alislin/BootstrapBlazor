01fc33f531ac5520b6a67821b8cf8326a0275d51
Argo-Lenovo <argo@163.com>
2022-03-15 17:16:12 +0800

    test: 增加设置默认值单元测试

--- test/UnitTest/Components/CascaderTest.cs
+++ test/UnitTest/Components/CascaderTest.cs
@@ -44,7 +44,7 @@ public class CascaderTest : BootstrapBlazorTestBase
             new() { Text = "Test1", Value = "1" },
             new() { Text = "Test2", Value = "2" }
         };
-        items[1].AddItem(new() { Text = "Test11", Value = "11" });
+        items[1].AddItem(new("11", "Test11"));
         items[1].AddItem(new() { Text = "Test12", Value = "12" });
 
         items[1].Items.ElementAt(1).AddItem(new() { Text = "Test111", Value = "111" });
@@ -82,5 +82,36 @@ public class CascaderTest : BootstrapBlazorTestBase
         linkItems = cut.FindAll(".nav-link");
         await cut.InvokeAsync(() => linkItems[1].Click());
         Assert.Equal("12", value);
+    }
+
+    [Fact]
+    public void SetDefaultValue_Ok()
+    {
+        var items = new List<CascaderItem>()
+        {
+            new() { Text = "Test1", Value = "1" },
+            new() { Text = "Test2", Value = "2" }
+        };
+        items[0].AddItem(new("11", "Test11"));
+
+        var cut = Context.RenderComponent<Cascader<string>>(pb =>
+        {
+            pb.Add(a => a.Items, items);
+            pb.Add(a => a.Value, "Test");
+        });
+        Assert.Equal("1", cut.Instance.Value);
+
+        cut.SetParametersAndRender(pb =>
+        {
+            pb.Add(a => a.Value, "11");
+        });
+        Assert.Equal("11", cut.Instance.Value);
+
+        cut.SetParametersAndRender(pb =>
+        {
+            pb.Add(a => a.Items, null);
+            pb.Add(a => a.Value, "1");
+        });
+        Assert.Empty(cut.Instance.Value);
     }
 }
