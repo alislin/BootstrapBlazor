9edba3fea52481f48e482a5c3b001f0e9dae76d7
Argo-Tianyi <argo@163.com>
2022-03-17 20:51:43 +0800

    test: 增加 Button 单元测试

--- test/UnitTest/Components/EditorTest.cs
+++ test/UnitTest/Components/EditorTest.cs
@@ -35,4 +35,44 @@ public class EditorTest : BootstrapBlazorTestBase
         await cut.InvokeAsync(() => cut.Instance.Update("Test1"));
         Assert.Equal("Test1", value.Name);
     }
+
+    [Fact]
+    public async Task CustomerToolbarButtons_Ok()
+    {
+        var cut = Context.RenderComponent<Editor>(pb =>
+        {
+            pb.Add(a => a.Value, "Test");
+            pb.Add(a => a.CustomerToolbarButtons, new EditorToolbarButton[]
+            {
+                new EditorToolbarButton()
+                {
+                    ButtonName = "Test1",
+                    IconClass = "Class1",
+                    Tooltip = "Tooltip1"
+                }
+            });
+        });
+
+        IEnumerable<object>? buttons = null;
+        await cut.InvokeAsync(async () => buttons = await cut.Instance.GetToolBar());
+        Assert.NotNull(buttons);
+
+        IEnumerable<EditorToolbarButton>? btns = null;
+        await cut.InvokeAsync(async () => btns = await cut.Instance.GetPluginAttrs());
+        Assert.Single(buttons);
+        Assert.Equal("Class1", btns!.First().IconClass);
+        Assert.Equal("Tooltip1", btns!.First().Tooltip);
+
+        var name = "";
+        cut.SetParametersAndRender(pb =>
+        {
+            pb.Add(a => a.OnClickButton, v =>
+            {
+                return Task.FromResult("Test");
+            });
+            pb.Add(a => a.Value, null);
+        });
+        await cut.InvokeAsync(async () => name = await cut.Instance.ClickPluginItem("Test1"));
+        Assert.Equal("Test", name);
+    }
 }
