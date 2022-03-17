59355e76795be20d957a525c736952253bd5e280
Argo-Tianyi <argo@163.com>
2022-03-17 20:51:19 +0800

    test: 增加单元测试

--- test/UnitTest/Components/EditorTest.cs
+++ test/UnitTest/Components/EditorTest.cs
@@ -8,4 +8,31 @@ namespace UnitTest.Components;
 
 public class EditorTest : BootstrapBlazorTestBase
 {
+    [Fact]
+    public async Task Editor_Ok()
+    {
+        var value = new Foo();
+        var cut = Context.RenderComponent<Editor>(pb =>
+        {
+            pb.Add(a => a.Value, value.Name);
+            pb.Add(a => a.ValueChanged, v => value.Name = v);
+            pb.Add(a => a.IsEditor, false);
+            pb.Add(a => a.Height, 200);
+        });
+
+        await cut.InvokeAsync(() => cut.Instance.Update("Test"));
+        Assert.Equal("Test", value.Name);
+
+        cut.SetParametersAndRender(pb =>
+        {
+            pb.Add(a => a.OnValueChanged, v =>
+            {
+                value.Name = v;
+                return Task.CompletedTask;
+            });
+        });
+
+        await cut.InvokeAsync(() => cut.Instance.Update("Test1"));
+        Assert.Equal("Test1", value.Name);
+    }
 }
