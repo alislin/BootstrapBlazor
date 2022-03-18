f79663294abf8a444167f10b3173b13fcff1e587
Argo-Tianyi <argo@163.com>
2022-03-18 18:00:31 +0800

    test: 增加 Download 单元测试

---
+++ test/UnitTest/Components/DownloadTest.cs
@@ -0,0 +1,11 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+using Microsoft.Extensions.DependencyInjection;
+
+namespace UnitTest.Components;
+
+public class DownloadTest : BootstrapBlazorTestBase
+{
+}
