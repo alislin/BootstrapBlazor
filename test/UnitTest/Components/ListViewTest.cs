44129169c1f65d9d04d7b8cf7365eb1d39c8a12c
Argo-Tianyi <argo@163.com>
2022-03-19 14:49:03 +0800

    test: 增加单元测试

--- test/UnitTest/Components/ListViewTest.cs
+++ test/UnitTest/Components/ListViewTest.cs
@@ -6,6 +6,27 @@ namespace UnitTest.Components;
 
 public class ListViewTest : BootstrapBlazorTestBase
 {
+    [Fact]
+    public void ListView_Ok()
+    {
+        var items = Enumerable.Range(1, 6).Select(i => new Product()
+        {
+            ImageUrl = $"images/Pic{i}.jpg",
+            Description = $"Pic{i}.jpg",
+            Category = $"Group{(i % 4) + 1}"
+        });
+        var cut = Context.RenderComponent<ListView<Product>>(pb =>
+        {
+            pb.Add(a => a.Items, items);
+            pb.Add(a => a.HeaderTemplate, builder => builder.AddContent(0, "Test-Header"));
+            pb.Add(a => a.BodyTemplate, p => builder => builder.AddContent(0, $"{p.ImageUrl}-{p.Description}-{p.Category}"));
+            pb.Add(a => a.FooterTemplate, builder => builder.AddContent(0, "Footer-Header"));
+        });
+        cut.Contains("Test-Header");
+        cut.Contains("Footer-Header");
+        cut.Contains("images/Pic1.jpg");
+    }
+
 
     private class Product
     {
