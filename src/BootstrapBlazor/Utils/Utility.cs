e16410a96d64d8e6813d444efaf311e3c3f27453
Argo-Lenovo <argo@163.com>
2022-03-20 23:49:54 +0800

    refactor: 重构代码

--- src/BootstrapBlazor/Utils/Utility.cs
+++ src/BootstrapBlazor/Utils/Utility.cs
@@ -260,7 +260,7 @@ public static class Utility
             builder.OpenComponent<Switch>(0);
             builder.AddAttribute(1, nameof(Switch.Value), fieldValue);
             builder.AddAttribute(2, nameof(Switch.IsDisabled), true);
-            builder.AddAttribute(3, nameof(Display<string>.DisplayText), displayName);
+            builder.AddAttribute(3, nameof(Switch.DisplayText), displayName);
             builder.CloseComponent();
         }
         else
